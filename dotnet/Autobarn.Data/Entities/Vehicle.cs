2db5c2415c2c2a84490b4f13c987c13d98b97885
Dylan Beattie <dylan@dylanbeattie.net>
2021-07-02 01:18:50 +0100

    Mark Vehicle.Year as NOT nullable (otherwise GraphQL will break later.)

--- dotnet/Autobarn.Data/Entities/Vehicle.cs
+++ dotnet/Autobarn.Data/Entities/Vehicle.cs
@@ -5,7 +5,7 @@ namespace Autobarn.Data.Entities {
 		public string Registration { get; set; }
 		public string ModelCode { get; set; }
 		public string Color { get; set; }
-		public int? Year { get; set; }
+		public int Year { get; set; }
 
 		public virtual Model VehicleModel { get; set; }
 	}
