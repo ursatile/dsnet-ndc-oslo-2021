510615a6a3a4c59210d2cccb577639f0d65df588
Dylan Beattoie <dylan@dylanbeattie.net>
2021-11-30 14:33:21 +0100

    PricingServer working using gRPC

--- dotnet/Autobarn.PricingClient/Program.cs
+++ dotnet/Autobarn.PricingClient/Program.cs
@@ -2,17 +2,22 @@
 using System.IO;
 using System.Threading.Tasks;
 using Autobarn.Messages;
+using Autobarn.PricingEngine;
 using EasyNetQ;
+using Grpc.Net.Client;
 using Microsoft.Extensions.Configuration;
 
 namespace Autobarn.PricingClient {
     class Program {
         private static readonly IConfigurationRoot config = ReadConfiguration();
-
+        private static Pricer.PricerClient grpcClient;
         static async Task Main(string[] args) {
             var amqp = config.GetConnectionString("AutobarnRabbitMQ");
             using var bus = RabbitHutch.CreateBus(amqp);
-            Console.WriteLine("Connected to bus! Listening for newVehicleMessages");
+            Console.WriteLine("Connected to bus; Listening for newVehicleMessages");
+            using var channel = GrpcChannel.ForAddress(config["AutobarnGrpcServerAddress"]);
+            grpcClient = new Pricer.PricerClient(channel);
+            Console.WriteLine("Connected to gRPC!");
             var subscriberId = $"Autobarn.AuditLog@{Environment.MachineName}";
             await bus.PubSub.SubscribeAsync<NewVehicleMessage>(subscriberId, HandleNewVehicleMessage);
             Console.WriteLine("Press Enter to exit");
@@ -20,7 +25,14 @@ namespace Autobarn.PricingClient {
         }
 
         private static async Task HandleNewVehicleMessage(NewVehicleMessage message) {
-            Console.WriteLine($"New vehicle message! {message.Registration}");
+            var priceRequest = new PriceRequest() {
+                Color = message.Color,
+                Manufacturer = message.ManufacturerName,
+                Model = message.ModelName,
+                Year = message.Year
+            };
+            var priceReply = await grpcClient.GetPriceAsync(priceRequest);
+            Console.WriteLine($"Vehicle {message.Registration} costs {priceReply.Price} {priceReply.CurrencyCode}");
         }
 
         private static IConfigurationRoot ReadConfiguration() {
