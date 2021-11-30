6d551587276ed37d9c816c382277e7101187494a
Dylan Beattoie <dylan@dylanbeattie.net>
2021-11-30 10:15:36 +0100

    Working GraphQL API with vehicles endpoint

---
+++ dotnet/Autobarn.Website/GraphQL/Schemas/AutobarnSchema.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Autobarn.Data;
+using Autobarn.Data.Entities;
+using Autobarn.Website.GraphQL.GraphTypes;
+using Autobarn.Website.GraphQL.Queries;
+using GraphQL;
+using GraphQL.Types;
+
+namespace Autobarn.Website.GraphQL.Schemas {
+    public class AutobarnSchema : Schema {
+        public AutobarnSchema(IAutobarnDatabase db) => Query = new VehicleQuery(db);
+
+    }
+}
+
+namespace Autobarn.Website.GraphQL.Queries {
+    public class VehicleQuery : ObjectGraphType {
+        private readonly IAutobarnDatabase db;
+
+        public VehicleQuery(IAutobarnDatabase db) {
+            this.db = db;
+            Field<ListGraphType<VehicleGraphType>>("Vehicles",
+                "Query to return every vehicle in the system",
+                resolve: GetAllVehicles);
+        }
+
+        private IEnumerable<Vehicle> GetAllVehicles(IResolveFieldContext<object> context) => db.ListVehicles();
+    }
+}
--- dotnet/Autobarn.Website/Startup.cs
+++ dotnet/Autobarn.Website/Startup.cs
@@ -5,7 +5,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using Autobarn.Website.GraphQL.Schemas;
 using EasyNetQ;
+using GraphiQl;
+using GraphQL.Server;
 
 namespace Autobarn.Website {
 	public class Startup {
@@ -35,6 +38,10 @@ namespace Autobarn.Website {
 
             var bus = RabbitHutch.CreateBus(Configuration.GetConnectionString("AutobarnRabbitMQ"));
             services.AddSingleton<IBus>(bus);
+
+            services.AddScoped<AutobarnSchema>();
+            services.AddGraphQL(options => options.EnableMetrics = false)
+                .AddNewtonsoftJson();
         }
 
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
@@ -50,6 +57,10 @@ namespace Autobarn.Website {
 			app.UseStaticFiles();
 			app.UseRouting();
 			app.UseAuthorization();
+
+
+            app.UseGraphQL<AutobarnSchema>();
+            app.UseGraphiQl("/graphiql");
 			app.UseEndpoints(endpoints => {
 				endpoints.MapControllerRoute(
 					name: "default",
